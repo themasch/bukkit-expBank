98407d89ca8932439fd27e5470cc940832b26fd8
Mark Schmale <masch@masch.it>
2012-09-06 18:26:30 +0200

    bump version number

--- src/it/masch/bukkit/expBank/ExpBankMain.java
+++ src/it/masch/bukkit/expBank/ExpBankMain.java
@@ -11,7 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class ExpBankMain  extends JavaPlugin
 {
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
     public static final String NAME = "expBank";
 
     private static final Logger log = Logger.getLogger("Minecraft");
